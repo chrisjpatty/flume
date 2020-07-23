1d80c8d8763ec2e406b56f5193ee20d2b20ca434
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2020-07-22 23:07:10 -0600

    First attempt at cached rects (non-functional)

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./Node.css";
-import { NodeTypesContext, NodeDispatchContext, StageContext } from "../../context";
+import {
+  NodeTypesContext,
+  NodeDispatchContext,
+  StageContext
+} from "../../context";
 import { getPortRect, calculateCurve } from "../../connectionCalculator";
-import { Portal } from 'react-portal'
-import ContextMenu from '../ContextMenu/ContextMenu'
+import { Portal } from "react-portal";
+import ContextMenu from "../ContextMenu/ContextMenu";
 import IoPorts from "../IoPorts/IoPorts";
-import Draggable from '../Draggable/Draggable'
+import Draggable from "../Draggable/Draggable";
 
 const Node = ({
   id,
@@ -30,19 +34,16 @@ const Node = ({
   const nodeWrapper = React.useRef();
   const [menuOpen, setMenuOpen] = React.useState(false);
   const [menuCoordinates, setMenuCoordinates] = React.useState({ x: 0, y: 0 });
+  const portCache = React.useRef({});
 
   const byScale = value => (1 / stageState.scale) * value;
 
-  const updateConnectionsByTransput = (transput = {}, isOutput) => {
+  const updateConnectionsByTransput = (transput = {}, isOutput, mouse) => {
     Object.entries(transput).forEach(([portName, outputs]) => {
       outputs.forEach(output => {
-        const toRect = getPortRect(id, portName, isOutput ? "output" : "input");
-        const fromRect = getPortRect(
-          output.nodeId,
-          output.portName,
-          isOutput ? "input" : "output"
-        );
-        const portHalf = fromRect.width / 2;
+        const toRect = portCache.current[id + portName];
+        const fromRect = portCache.current[output.nodeId + output.portName];
+        const portHalf = fromRect.halfWidth;
         let combined;
         if (isOutput) {
           combined = id + portName + output.nodeId + output.portName;
@@ -52,24 +53,78 @@ const Node = ({
         const cnx = document.querySelector(
           `[data-connection-id="${combined}"]`
         );
+        const xDiff = x - mouse.x
+        const yDiff = y - mouse.y
         const from = {
-          x: byScale(toRect.x - stageRect.current.x + portHalf - (stageRect.current.width / 2)) + byScale(stageState.translate.x),
-          y: byScale(toRect.y - stageRect.current.y + portHalf - (stageRect.current.height / 2)) + byScale(stageState.translate.y)
-        }
+          x:
+            byScale(
+              toRect.x - xDiff -
+                stageRect.current.x +
+                portHalf -
+                stageRect.current.width / 2
+            ) + byScale(stageState.translate.x),
+          y:
+            byScale(
+              toRect.y - yDiff -
+                stageRect.current.y +
+                portHalf -
+                stageRect.current.height / 2
+            ) + byScale(stageState.translate.y)
+        };
         const to = {
-          x: byScale(fromRect.x - stageRect.current.x + portHalf - (stageRect.current.width / 2)) + byScale(stageState.translate.x),
-          y: byScale(fromRect.y - stageRect.current.y + portHalf - (stageRect.current.height / 2)) + byScale(stageState.translate.y)
-        }
-        cnx.setAttribute("d", calculateCurve(from, to))
+          x:
+            byScale(
+              fromRect.x -
+                stageRect.current.x +
+                portHalf -
+                stageRect.current.width / 2
+            ) + byScale(stageState.translate.x),
+          y:
+            byScale(
+              fromRect.y -
+                stageRect.current.y +
+                portHalf -
+                stageRect.current.height / 2
+            ) + byScale(stageState.translate.y)
+        };
+        cnx.setAttribute("d", calculateCurve(from, to));
       });
     });
   };
 
-  const updateNodeConnections = () => {
+  const updateNodeConnections = (coordinates = {x: 0, y: 0}) => {
     if (connections) {
-      updateConnectionsByTransput(connections.inputs);
-      updateConnectionsByTransput(connections.outputs, true);
+      updateConnectionsByTransput(connections.inputs, false, coordinates);
+      updateConnectionsByTransput(connections.outputs, true, coordinates);
     }
+  };
+
+  const cachePortsByTransput = (transput, isOutput) => {
+    Object.entries(transput).forEach(([portName, outputs]) => {
+      outputs.forEach(output => {
+        const toRect = getPortRect(id, portName, isOutput ? "output" : "input");
+        const fromRect = getPortRect(
+          output.nodeId,
+          output.portName,
+          isOutput ? "input" : "output"
+        );
+        portCache.current[id + portName] = {
+          x: toRect.x,
+          y: toRect.y,
+          halfWidth: toRect.width / 2
+        };
+        portCache.current[output.nodeId + output.portName] = {
+          x: fromRect.x,
+          y: fromRect.y,
+          halfWidth: fromRect.width / 2
+        };
+      });
+    });
+  };
+
+  const cachePortRects = () => {
+    cachePortsByTransput(connections.inputs);
+    cachePortsByTransput(connections.outputs, true);
   };
 
   const stopDrag = (e, coordinates) => {
@@ -80,13 +135,14 @@ const Node = ({
     });
   };
 
-  const handleDrag = ({x, y}) => {
+  const handleDrag = ({ x, y }) => {
     nodeWrapper.current.style.transform = `translate(${x}px,${y}px)`;
-    updateNodeConnections()
-  }
+    updateNodeConnections({ x, y });
+  };
 
   const startDrag = e => {
-    onDragStart()
+    cachePortRects();
+    onDragStart();
   };
 
   const handleContextMenu = e => {
@@ -103,16 +159,20 @@ const Node = ({
 
   const handleMenuOption = ({ value }) => {
     switch (value) {
-      case 'deleteNode':
+      case "deleteNode":
         nodesDispatch({
           type: "REMOVE_NODE",
           nodeId: id
-        })
+        });
         break;
       default:
         return;
     }
-  }
+  };
+
+  React.useEffect(() => {
+    cachePortRects()
+  }, [])
 
   return (
     <Draggable
@@ -145,9 +205,15 @@ const Node = ({
             x={menuCoordinates.x}
             y={menuCoordinates.y}
             options={[
-              ...(deletable !== false ?
-              [{label: "Delete Node", value: "deleteNode", description: "Deletes a node and all of its connections."}]
-              : [])
+              ...(deletable !== false
+                ? [
+                    {
+                      label: "Delete Node",
+                      value: "deleteNode",
+                      description: "Deletes a node and all of its connections."
+                    }
+                  ]
+                : [])
             ]}
             onRequestClose={closeContextMenu}
             onOptionSelected={handleMenuOption}
