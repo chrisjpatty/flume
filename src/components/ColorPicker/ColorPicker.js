f886487a72453c8941fa46874e725d1bd7f55aad
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2020-07-27 14:08:28 -0600

    🐛 Color picker now closes on escape and click outside

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -3,8 +3,43 @@ import styles from "./ColorPicker.css";
 import { Colors } from "../../typeBuilders";
 
 export default ({ x, y, onColorPicked, onRequestClose }) => {
+  const wrapper = React.useRef();
+
+  const testClickOutside = React.useCallback(
+    e => {
+      if (!wrapper.current.contains(e.target)) {
+        onRequestClose();
+        document.removeEventListener("click", testClickOutside);
+        document.removeEventListener("contextmenu", testClickOutside);
+      }
+    },
+    [wrapper, onRequestClose]
+  );
+
+  const testEscape = React.useCallback(
+    e => {
+      if (e.keyCode === 27) {
+        onRequestClose();
+        document.removeEventListener("keydown", testEscape);
+      }
+    },
+    [onRequestClose]
+  );
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", testEscape);
+    document.addEventListener("click", testClickOutside);
+    document.addEventListener("contextmenu", testClickOutside);
+    return () => {
+      document.removeEventListener("click", testClickOutside);
+      document.removeEventListener("contextmenu", testClickOutside);
+      document.removeEventListener("keydown", testEscape);
+    };
+  }, [testClickOutside, testEscape]);
+
   return (
     <div
+      ref={wrapper}
       className={styles.wrapper}
       style={{
         left: x,
