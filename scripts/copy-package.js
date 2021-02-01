f416b9a80fcbd0122063a05941999cd63fda4a94
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2021-01-31 18:15:03 -0700

    🐛 Now uses "files" key instead of custom deploy script

--- scripts/copy-package.js
+++
@@ -1,8 +0,0 @@
-const { readFileSync, writeFileSync } = require('fs')
-
-const main = () => {
-  const pkg = readFileSync("package.json", { encoding: "utf8" }).replace(/dist\//g, "")
-  writeFileSync("dist/package.json", pkg, { encoding: "utf8"})
-}
-
-main()
