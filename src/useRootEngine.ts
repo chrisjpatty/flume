89bd4c57c526402442916e45cea9b86189be8644
Christopher Patty <christopher.jon.patty@gmail.com>
2023-03-18 17:43:07 -0600

    Context is now an optional prop of useRootEngine

--- src/useRootEngine.ts
+++ src/useRootEngine.ts
@@ -4,8 +4,8 @@ import { NodeMap, RootEngineOptions } from "./types";
 export const useRootEngine = <T extends { [inputName: string]: any }>(
   nodes: NodeMap,
   engine: RootEngine,
-  context: any,
-  options: RootEngineOptions = {}
+  context?: any,
+  options?: RootEngineOptions
 ): T =>
   Object.keys(nodes).length
     ? engine.resolveRootNode<T>(nodes, { ...options, context })
